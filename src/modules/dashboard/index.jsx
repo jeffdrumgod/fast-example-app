10b1bca0128e03430efa736d6d07a72c8d5f1db8
Jefferson Rafael Kozerski <jeff.drumgod@gmail.com>
2017-08-19 18:47:05 -0300

    🗑  fix eslint warn

--- src/modules/dashboard/index.jsx
+++ src/modules/dashboard/index.jsx
@@ -2,26 +2,24 @@ import Modules from 'Modules/index.js';
 
 const { Link } = ReactRouterDOM;
 
-const dashboard = () => {
-	return (
-		<div>
-			<h1>Dashboard - Ready!</h1>
-			<p>Modules: </p>
-			<ul>
-				{Object.keys(Modules).map((item) => {
-					if (_.includes(['ready', 'moduleBase', 'dashboard'], item)) {
-						return false;
-					}
+const dashboard = () => (
+	<div>
+		<h1>Dashboard - Ready!</h1>
+		<p>Modules: </p>
+		<ul>
+			{Object.keys(Modules).map((item) => {
+				if (_.includes(['ready', 'moduleBase', 'dashboard'], item)) {
+					return false;
+				}
 
-					return (
-						<li key={item}>
-							<Link to={`/${item}`}>{item}</Link>
-						</li>
-					);
-				})}
-			</ul>
-		</div>
-	);
-};
+				return (
+					<li key={item}>
+						<Link to={`/${item}`}>{item}</Link>
+					</li>
+				);
+			})}
+		</ul>
+	</div>
+);
 
 export default dashboard;
