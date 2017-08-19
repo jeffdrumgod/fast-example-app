fd23b32c6388d3e54d471ce13c5e6446dfbdc7ae
Jefferson Rafael Kozerski <jeff.drumgod@gmail.com>
2017-08-19 18:39:11 -0300

    ➕  Added list of loaded modules

--- src/modules/dashboard/index.jsx
+++ src/modules/dashboard/index.jsx
@@ -1,5 +1,26 @@
-const dashboard = () => (
-	<h1>Dashboard - Ready!</h1>
-);
+import Modules from 'Modules/index.js';
+
+const dashboard = () => {
+	console.log(Modules);
+	return (
+		<div>
+			<h1>Dashboard - Ready!</h1>
+			<p>Modules: </p>
+			<ul>
+				{Object.keys(Modules).map((item) => {
+					if (_.includes(['ready', 'moduleBase', 'dashboard'], item)) {
+						return false;
+					}
+
+					return (
+						<li key={item}>
+							<a href={`/${item}`}>{item}</a>
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
+};
 
 export default dashboard;
