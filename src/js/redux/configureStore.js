2f95b6cd4f6f540c16a7474b7a76c2fc6ab5bf20
Jefferson Rafael Kozerski <jeff.drumgod@gmail.com>
2017-08-20 15:17:28 -0300

    ➕  Added instanceName to debug in ReduxExtension

--- src/js/redux/configureStore.js
+++ src/js/redux/configureStore.js
@@ -2,7 +2,7 @@ const { createStore, applyMiddleware, compose } = Redux;
 const { routerMiddleware } = ReactRouterRedux;
 const createSagaMiddleware = ReduxSaga.default;
 
-export default function createStoreWithMiddleware(reducer, rootSaga, history, data) {
+export default function createStoreWithMiddleware(reducer, instanceName = 'Reducer Base', rootSaga, history, data = {}) {
 	const reduxRouterMiddleware = routerMiddleware(history);
 	const sagaMiddleware = createSagaMiddleware();
 	const middleware = [
@@ -10,7 +10,13 @@ export default function createStoreWithMiddleware(reducer, rootSaga, history, da
 		sagaMiddleware,
 	];
 	/* eslint-disable no-underscore-dangle */
-	const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+	let composeEnhancers = compose;
+	if (!!window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && !!instanceName) {
+		composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+			name: instanceName
+		});
+	}
+
 	const store = createStore(
 		reducer,
 		data,
